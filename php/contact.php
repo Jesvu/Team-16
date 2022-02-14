a05eb1f9d8bcd857cfe093e289b3f5543c5af7e1
Jesvu <91062760+Jesvu@users.noreply.github.com>
2022-02-14 19:08:54 +0200

    Lomake done

--- php/contact.php
+++ php/contact.php
@@ -1,15 +1,16 @@
 <?php 
-if(isset($_POST['submit']) && $_POST['submit'] = ""){
-   
+if(isset($_POST['submit'])){
    $email = $_POST['email'];
    $pNumber = $_POST['pNumber'];
    $subject = $_POST['subject'];
    $message = $_POST['message'];
    
-    $to = "jessevuorela@hotmail.com"; // this is your Email address
+    $to = "jesse21008@student.hamk.fi";
     $headers = "From: " .$email;
+    $txt = "Sender Phonenumber: " .$pNumber.
+            "\nMessage: " .$message;
 
-   mail($to, $subject, $message, $headers);
-   header("Location: ../index.html");
+   mail($to, $subject, $txt, $headers);
+   header("Location:../index.html");
 }
 ?>
