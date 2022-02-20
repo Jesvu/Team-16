360273a0579b1362f155e68364565c5d255996e2
Taneli47 <taneli.nikkanen@student.hamk.fi>
2022-02-20 16:26:38 +0200

    feedback added

--- php/feedback.php
+++ php/feedback.php
@@ -1,12 +1,34 @@
 <?php
 session_start();
 if (!isset($_SESSION["users"])){
-    header("Location:../login.html");
+    
     exit;
 }
-print "<h2>Tervetuloa, ".$_SESSION["users"]."!</h2>";
+print "<h2>Olet kirjautuneena nimellä: ".$_SESSION["users"]."!</h2>";
 ?>
-<form>
-feedback: <input type="text" name="feedback" style="height:30px; width" placeholder="feedback..."><br>
-</form>
-<a href='kirjauduulos.php'>Kirjaudu ulos</a>
+
+<?php 
+if(isset($_POST['submit'])){
+   $email = $_POST['email'];
+   $pNumber = $_POST['pNumber'];
+   $subject = $_POST['subject'];
+   $message = $_POST['message'];
+
+   if (empty($email) || empty($pNumber) || empty($subject) || empty($message)){
+         
+         exit;
+      }
+   //Valitaan vastaanottajan sposti
+   $to = "taneli21000@student.hamk.fi";
+   //Tekstikentän tiedot, mitä lähetetään
+   $headers = "From: " .$email;
+   $txt = "Sender Phonenumber: " .$pNumber.
+            "\nMessage: " .$message;
+   //spostin lähetys
+   mail($to, $subject, $txt, $headers);
+   //Takaisin pääsivulle
+   
+}
+?>
+<a href='../feedback.html'>Leave feedback</a>
+<a href='logout.php'>Log out</a>
